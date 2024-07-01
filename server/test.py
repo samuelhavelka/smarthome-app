0b361b7f658572e795d641f965b0f2760a10f2bc
samuelhavelka <125111579+samuelhavelka@users.noreply.github.com>
2024-07-01 11:27:03 -0500

    Delete server/test.py

--- server/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-test = np.random.normal(0.5,0.2,20).round(3)
-numbers = []
-for i in test:
-    if i > 0.1 and i < 0.9: numbers.append(i)
-numbers = sorted(list(set(numbers)))
-
-print(numbers)
